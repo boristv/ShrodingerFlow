f388ba83b237d4ec756fee5b7db631f1d198cc2b
BorisTV <boristlkn@gmail.com>
2023-07-02 17:37:33 +0300

    refactoring

--- Assets/Scenes/DOTS/SFDOTS.cs
+++ Assets/Scenes/DOTS/SFDOTS.cs
@@ -8,92 +8,68 @@ using source;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using float3 = Unity.Mathematics.float3;
-using Matrix4x4 = UnityEngine.Matrix4x4;
-using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
-using Vector3 = UnityEngine.Vector3;
 
 public class SFDOTS : MonoBehaviour
 {
-    
-    private SystemHandle _system;
-    private World _world;
-    
-    public static float3[] p;
-    ParticleSystem.Particle[] cloud;
-    
-    bool bPointsUpdated = false;
     //PARAMETERS
     [Header("Начальные условия")]
     [SerializeField, Tooltip("Box size")] private int[] vol_size = { 10, 5, 5 };      // box size
     [SerializeField, Tooltip("Volume resolution")] private int[] vol_res = { 128, 64, 64 };    // volume resolution
     [SerializeField, Tooltip("Planck constant")] private float hbar = (float)0.1;           // Planck constant
     [SerializeField, Tooltip("Time step")] private float dt = 1 / (float)12;          // time step
-    int tmax = 85;
     [SerializeField, Tooltip("Background velocity")] private float[] background_vel = {(float) -0.2, 0, 0};
-        
-    
     [SerializeField, Tooltip("First ring radius")] private float r1 = (float)1.5; 
     [SerializeField, Tooltip("Second ring radius")] private float r2 = (float)0.9;              
     [SerializeField] private float[] n1 = {-1,0,0};         
     [SerializeField] private float[] n2 = {-1,0,0};
-
-    [SerializeField, Tooltip("Particles count")] private int n_particles = 100000;
-
-    Velocity vel;
-    object lk= new object();
-
-    UnityThreading.ActionThread myThread;
-
-    [Header("Дополнительные настройки")] 
-    [SerializeField, Tooltip("Размер частиц")] private float _particleSize = 0.1f;
     
     [Header("Начальное расположение частиц")] 
     [SerializeField] private Vector2 _boxSizeX = new Vector2(5, 5);
     [SerializeField] private Vector2 _boxSizeY = new Vector2(0.5f, 4.5f);
     [SerializeField] private Vector2 _boxSizeZ = new Vector2(0.5f, 4.5f);
 
-    [SerializeField] private Mesh _mesh;
-    [SerializeField] private Material _material;
-
-    private ObjectPositionJob _job;
-    private NativeArray<float3> _nativeP;
-    private NativeArray<float> _nativeCubeYOffsets;
-    private NativeArray<Matrix4x4> _nativeMatrices;
+    [Header("Дополнительные настройки")] 
+    [SerializeField, Tooltip("Particles count")] public int n_particles = 100000;
+    [SerializeField, Tooltip("Размер частиц")] private float _particleSize = 0.1f;
+    
+    public static float3[] Positions;
+    
+    private ParticleSystem.Particle[] cloud;
+    private int tmax = 85;
+    private bool bPointsUpdated = false;
+    
+    private SystemHandle _system;
+    private World _world;
+    private UnityThreading.ActionThread myThread;
+    private Velocity vel;
+    private object lk= new object();
     private NativeArray<float3> _nativePositions;
-    private RenderParams _rp;
-
     private bool _isReady;
+
+    public static SFDOTS Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     
     private System.Collections.IEnumerator Start()
     {
+        Positions = new float3[n_particles];
+        
+        var count = n_particles;
+        _nativePositions = new NativeArray<float3>(count, Allocator.Persistent);
+        
+        myThread = UnityThreadHelper.CreateThread(Worker);
+        
         yield return new WaitForSeconds(0.5f);
 
         _world = World.DefaultGameObjectInjectionWorld;
         _system = _world.CreateSystem<SpawnerSystem>();
-        
-        p = new float3[n_particles];
-        
-        _rp = new RenderParams(_material);
-        
-        var count = n_particles;
-        _nativePositions = new NativeArray<float3>(count, Allocator.Persistent);
-        _nativeMatrices = new NativeArray<Matrix4x4>(count, Allocator.Persistent);
-        _nativeCubeYOffsets = new NativeArray<float>(count, Allocator.Persistent);
-        _nativeP = new NativeArray<float3>(count, Allocator.Persistent);
-        
-        _job = new ObjectPositionJob
-        {
-            Positions = _nativePositions,
-            Size = _particleSize * Vector3.one,
-            Rotation = Quaternion.identity
-        };
-        
-        myThread = UnityThreadHelper.CreateThread(Worker);
     }
 
     private void Worker()
@@ -169,7 +145,7 @@ public class SFDOTS : MonoBehaviour
 
             for (int ii = 0; ii < n_particles; ++ii)
             {
-                p[ii] = new float3(px[ii], py[ii], pz[ii]);
+                Positions[ii] = new float3(px[ii], py[ii], pz[ii]);
             }
 
             lock (lk)
@@ -199,14 +175,9 @@ public class SFDOTS : MonoBehaviour
     }
 }
 
-public struct SpawnerPosition : IComponentData
+public struct InitialData : IComponentData
 {
-    public float3 Value;
-    public int Index;
-}
-
-public struct IndexComponent : IComponentData
-{
+    public float3 SpawnerPosition;
     public int Index;
 }
 
@@ -215,6 +186,7 @@ public struct IndexComponent : IComponentData
 public partial struct SpawnerSystem : ISystem
 {
     private NativeArray<float3> _nativePositions;
+    private int count;
     
     public void OnCreate(ref SystemState state)
     {
@@ -222,7 +194,7 @@ public partial struct SpawnerSystem : ISystem
         var scale = SystemAPI.GetComponent<LocalTransform>(prefab).Scale * 0.1f;
         var buffer = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
         
-        var count = 100000;
+        count = SFDOTS.Instance.n_particles;
         
         _nativePositions = new NativeArray<float3>(count, Allocator.Persistent);
 
@@ -230,31 +202,20 @@ public partial struct SpawnerSystem : ISystem
         {
             var entity = buffer.Instantiate(prefab);
             var pos = float3.zero;
-            buffer.AddComponent(entity, new SpawnerPosition { Value = pos.y, Index = i});
-            //buffer.AddComponent(entity, new IndexComponent() { Index = i });
+            buffer.AddComponent(entity, new InitialData() { SpawnerPosition = pos.y, Index = i});
             buffer.SetComponent(entity, LocalTransform.FromPosition(pos).ApplyScale(scale));
         }
-
-        //int i = 0;
-        //MovingController.SetPositions((_, pos) =>
-        //{
-        //    var entity = buffer.Instantiate(prefab);
-        //    buffer.AddComponent(entity, new SpawnerPosition { Value = pos.y, Index = i++});
-        //    buffer.SetComponent(entity, LocalTransform.FromPosition(pos).ApplyScale(scale));
-        //});
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        var p = SFDOTS.p;
-        for (int ii = 0; ii < 100000; ii++)
+        var p = SFDOTS.Positions;
+        for (int i = 0; i < count; i++)
         {
-            _nativePositions[ii] = p[ii];
+            _nativePositions[i] = p[i];
         }
-        var job = new SpawnerJob {Moment = (float) SystemAPI.Time.ElapsedTime};
-        job.Positions = _nativePositions;
-        job.ScheduleParallel(state.Dependency).Complete();
+        new SpawnerJob {Positions = _nativePositions}.ScheduleParallel(state.Dependency).Complete();
     }
 }
 
@@ -262,29 +223,25 @@ public partial struct SpawnerSystem : ISystem
 [StructLayout(LayoutKind.Auto)]
 public partial struct SpawnerJob : IJobEntity
 {
-    public float Moment;
     [ReadOnly] public NativeArray<float3> Positions;
 
     [BurstCompile]
     private void Execute(SpawnerAspect aspect)
     {
-        aspect.UpdateLocalTransformFromSpawnerPosition(Moment, Positions[aspect.Index]);
+        aspect.UpdateLocalTransformFromSpawnerPosition(Positions[aspect.Index]);
     }
 }
 
 public readonly partial struct SpawnerAspect : IAspect
 {
     private readonly RefRW<LocalTransform> _localTransform;
-    private readonly RefRO<SpawnerPosition> _targetPosition;
+    private readonly RefRO<InitialData> _targetPosition;
 
     public readonly int Index => _targetPosition.ValueRO.Index;
 
-    public void UpdateLocalTransformFromSpawnerPosition(float moment, float3 pos)
+    public void UpdateLocalTransformFromSpawnerPosition(float3 pos)
     {
-        //(_localTransform.ValueRW.Position, _localTransform.ValueRW.Rotation) =
-         //   _localTransform.ValueRW.Position.CalculatePosBurst(_targetPosition.ValueRO.Value.y, moment);
         _localTransform.ValueRW.Position = pos;
-        //_localTransform.ValueRW.Scale = 0.1f;
         _localTransform.ValueRW.Rotation = quaternion.identity;
     }
 }
